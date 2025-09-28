d5f802c0af3e215ee92638287f010784ecdd73a5
GoldOfLeaves <goldenleaf942@gmail.com>
2025-09-28 23:30:43 +0800

    i didnt code them yet theyre only for show

---
+++ jokers/casette.lua
@@ -0,0 +1,8 @@
+SMODS.Joker({
+	key = "casette",
+	rarity = 2,
+	pos = {x=4,y=1},
+	atlas = "medium_jokers",
+	calculate = function(self, card, context)
+	end
+})
---
+++ jokers/sunrise.lua
@@ -0,0 +1,8 @@
+SMODS.Joker({
+	key = "sunrise",
+	rarity = 2,
+	pos = {x=5,y=1},
+	atlas = "medium_jokers",
+	calculate = function(self, card, context)
+	end
+})
